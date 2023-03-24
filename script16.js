69448a9c9c25b814de92f952f718a628551a583b
Minskoleopc <minskoleopc@gmail.com>
2023-03-24 22:34:51 +0530

    added

--- script16.js
+++ script16.js
@@ -82,10 +82,91 @@ console.log(numC)
 console.log(numD)
 
 country = ["India","bangladesh","srilanka","china"]
-
-
 let q66 = country.at(2)
 console.log(q66)
+
+
+// filter() , reduce(), map() , forEach() , find() , findIndex() , every() , some()
+
+let birthYear = [1989,1990,1991,1992]
+// [34,33,32,31]
+let ages = [] // [34,33,32,31]
+for(let i = 0 ; i < birthYear.length ; i++){
+    //console.log(i)
+    //console.log(birthYear[i])
+    //console.log(2023 - birthYear[i])
+    let a = 2023 - birthYear[i]
+    ages.push(a)
+}
+console.log(ages)
+
+let q1 = birthYear.map(function(el,index,arr){
+    //console.log(el,index,arr)
+    return 2023 - el
+})
+
+console.log(q1)
+
+
+let tables = [1,2,3,4,5,6,7,8,9,10]
+// [2,,4,6,8,9,10,12,14,16,18,20]
+let q22 = tables.map(function(el){
+    return el * 2
+})
+console.log(q22)
+
+
+// program 2
+let marks  = [44,55,66,77,33,88,99,44]
+//[66,77,88,99]
+let above60 = [] // [66]
+for(let i = 0 ; i < marks.length ; i++){
+    // console.log(i)
+    // console.log(marks[i])
+    if(marks[i] > 60){
+        above60.push(marks[i])
+    }
+}
+console.log(above60)
+
+// filter
+let q333 = marks.filter(function(el){
+    return el > 60
+})
+console.log(q333)
+
+
+// program 3
+
+let numbers = [11,22,33]
+let total = 0
+
+for(let i = 0 ; i < numbers.length ; i++){
+    // console.log(i)
+    // console.log(numbers[i])
+
+    total = total + numbers[i]
+    //        0    +  11   ======> 11
+    //        11   +  22   ======> 33
+    //        33  +  33    ======>  66
+}
+console.log(total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
