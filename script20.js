8631a65c7fdc64cde379f39d7e15a47cd15ca9c1
Minskoleopc <minskoleopc@gmail.com>
2023-03-30 22:03:59 +0530

    added

---
+++ script20.js
@@ -0,0 +1,71 @@
+// object literal
+let chinmay = {
+    firstName:"chinmay",
+    lastName:"deshpande",
+    rollNo:34,
+    age:32
+}
+
+let amol = {
+    firstName:"amol",
+    lastName:"rao",
+    rollNo:22,
+    age:17
+}
+// 100 objects --- 500 lines
+
+// retrive (.dot notation and bracket notation)
+
+// console.log(amol.firstName)
+// console.log(amol['firstName'])
+// // update (.dot notation and bracket notation)
+// amol.firstName = "amol r"
+// amol['lastName'] = "rao"
+// // add (.dot notation and bracket notation)
+// amol.city = "pune"
+// amol['language'] = "hindi"
+// // delete (.dot notation and bracket notation)
+
+// delete amol['firstName']
+// delete amol['lastName']
+
+
+// class Person {
+//     firstName = "chinmay"
+//     lastName = "deshpande"
+//     age = 23
+//     rollNo = 22
+// }
+// let chinmay2 = new  Person()
+// let sarika2 = new  Person()
+// console.log(chinmay2)
+// console.log(sarika2)
+
+// sarika2.firstName = "sarika"
+// sarika2.lastName = "pansare"
+// sarika2.age = 12
+// sarika2.rollNo = 33
+
+// console.log(sarika2)
+// console.log(chinmay2)
+
+
+class PersonC {
+    constructor(fn,ln, age,rn){
+        this.firstName = fn
+        this.lastName = ln
+        this.age = age
+        this.rollNo = rn
+    }
+}
+
+let chinmay3 = new PersonC("chinmay3","deshpande3",12,14)
+console.log(chinmay3)
+
+
+
+
+
+
+
+
