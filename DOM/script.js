0a5d4d8502322b823aad53d7a370db7f8d4cd7f5
Minskoleopc <minskoleopc@gmail.com>
2023-04-08 22:32:19 +0530

    added

---
+++ DOM/script.js
@@ -0,0 +1,7 @@
+// number , boolean , string , map , set , userdefined ,array , object
+// htmlelement
+
+let headOne = document.querySelector('h1')
+headOne.addEventListener('click',function(){
+    headOne.style.color = "red";
+})
